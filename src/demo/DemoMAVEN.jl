7a2953e4f1475409b8b8db6dc3135b242fc5d9da
sjzhao <sjzhao2025@mail.ustc.edu.cn>
2026-04-19 14:34:45 +0800

    新建Demo文件夹

--- src/demo/DemoMAVEN.jl
+++ src/demo/DemoMAVEN.jl
@@ -13,4 +13,3 @@ file_name = "mvn_mag_l2_2022236ss_20220824_v01_r01.sts"
 mag2c32hz = load_mag_l2(joinpath(data_path, file_name))
 mag2c32hz[:position] .= mag2c32hz[:position] ./ 3390.0
 println("Data loaded.")
-
